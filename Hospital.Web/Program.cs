07999f12ec7c774ef09a4b6137f82a2606d1087d
Lenovo <Lenovo@DESKTOP-S6MTP5J>
2025-01-07 01:27:22 +0200

    AddSwagger

--- Hospital.Web/Program.cs
+++ Hospital.Web/Program.cs
@@ -1,5 +1,13 @@
+using Hospital.Application.Repositories;
+using Hospital.Domain.Repositories;
+using Hospital.Infrastructure;
 using Hospital.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Microsoft.OpenApi.Models;
+using Hospital.Infrastructure;
+using NuGet.Protocol.Core.Types;
+using Octokit;
 
 namespace Hospital.Web
 {
@@ -10,13 +18,28 @@ namespace Hospital.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
             builder.Services.AddControllersWithViews();
+            builder.Services.AddEndpointsApiExplorer();
 
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
+            builder.Services.AddScoped<ApplicationDbContext>();
 
+            //builder.Services.AddScoped(typeof(IRepository<>), typeof(IRepository<>));
+
+            // Register the specific patient repository
+            builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+            builder.Services.AddResponseCompression();
+
+            //add swagger
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v2", new OpenApiInfo { Title = "MVCCallWebAPI", Version = "v2" });
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -26,6 +49,12 @@ namespace Hospital.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            //add swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "MVCCallWebAPI");
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
