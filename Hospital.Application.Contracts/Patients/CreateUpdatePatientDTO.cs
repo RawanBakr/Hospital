6b95d1f1b7be9a3363e54592976452887039d5a8
Lenovo <Lenovo@DESKTOP-S6MTP5J>
2025-01-11 03:18:39 +0200

    AddProjectApplicationContracts

---
+++ Hospital.Application.Contracts/Patients/CreateUpdatePatientDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Application.Contracts.Patients;
+
+public class CreateUpdatePatientDTO
+{
+    public Guid ID { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Gender { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string UserName { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Password { get; set; }
+}
---
+++ Hospital.Application.Contracts/Patients/PatientDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Application.Contracts.Patients;
+
+public class PatientDTO
+{
+    public Guid Id { get; set; }
+
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MaxLength(100)]
+    public string Gender { get; set; }
+
+    [MaxLength(100)]
+    public string UserName { get; set; }
+
+    [MaxLength(100)]
+    public string Password { get; set; }
+}
