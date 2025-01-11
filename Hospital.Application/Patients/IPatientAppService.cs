a40c2c489712ff027f1f78087648dc5c443333b2
Lenovo <Lenovo@DESKTOP-S6MTP5J>
2025-01-11 03:16:58 +0200

    AddPatientService

---
+++ Hospital.Application/Patients/IPatientAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Application.Patients;
+
+public interface IPatientAppService<PatientDto, Guid, CreateUpdatePatientDTO>
+{
+    Task<IEnumerable<PatientDto>> GetPatientList(PatientDto patientDto);
+    Task<PatientDto> CreatePatient(CreateUpdatePatientDTO createPatient);
+    Task UpdatePatient(Guid id, CreateUpdatePatientDTO updatePatient);
+    Task<PatientDto> GetPatientId(Guid id);
+
+}
